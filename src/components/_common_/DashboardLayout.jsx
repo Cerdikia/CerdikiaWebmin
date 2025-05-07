824ef8fb0be9418136b744b61608dd84e6da27fa
raffiMRG <mutekinoraffi@gmail.com>
2025-05-07 16:14:40 +0700

    v 0 fahmi thebest

--- src/components/_common_/DashboardLayout.jsx
+++ src/components/_common_/DashboardLayout.jsx
@@ -1,15 +1,27 @@
-import Sidebar from "./Sidebar";
-import { Outlet } from "react-router-dom";
+import { Suspense } from "react"
+import { Outlet } from "react-router-dom"
+import Sidebar from "./Sidebar"
 
 export default function DashboardLayout() {
   return (
-    <div className="flex">
+    <div className="flex bg-gray-50 min-h-screen">
       <Sidebar />
-      {/* <div className="flex-1 p-8 pt-16 ml-0 md:ml-64"> */}
-      <div className="flex-1 p-8 pt-16 ml-0">
-        {/* Semua halaman (content) akan ditampilkan di sini */}
-        <Outlet />
+      <div className="flex-1 lg:ml-72">
+        <main className="p-6">
+          <Suspense
+            fallback={
+              <div className="flex items-center justify-center h-screen">
+                <div className="flex flex-col items-center">
+                  <div className="w-12 h-12 border-4 border-indigo-500 border-t-transparent rounded-full animate-spin"></div>
+                  <p className="mt-4 text-gray-700">Loading...</p>
+                </div>
+              </div>
+            }
+          >
+            <Outlet />
+          </Suspense>
+        </main>
       </div>
     </div>
-  );
+  )
 }
